d89f1ff98b5925e9a18056e806339ba213fe2d1e
luobozz <437337961@qq.com>
2021-08-21 11:05:17 +0800

    add hyper

---
+++ hyper/install.bat
@@ -0,0 +1,8 @@
+hyper i verminal
+hyper i hyper-search
+hyper i hyper-opacity
+hyper i hyperterm-dibdabs
+hyper i hyper-dark-scrollbar
+hyper i hyper-statusline-extended
+hyper i hyper-mac-controls
+hyper i hyper-dnd-tabs
