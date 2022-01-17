d35b254d5f048a064be40b6f403af92f788a7520
luobo <437337961@qq.com>
2022-01-17 18:34:20 +0800

    dwm

--- dwm/dwm/config.def.h
+++ dwm/dwm/config.def.h
@@ -1,23 +1,39 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 320;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char normbgcolor[]           = "#222222";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+//窗口边框宽度
+static const unsigned int borderpx  = 3;
+// 字体
+static const char *fonts[]          = { "Fira Mono:style=Bold:size=10","HarmonyOS Sans SC:style=Bold:size=10" };
+static const char dmenufont[]       = "Fira Mono:style=Bold:size=10";
+
+//颜色
+//未选中前景
+static const char col_fg[]       = "#fffffb";
+static const char col_bg[]       = "#323742";
+static const char col_border[]       = "#323742";
+
+//选中前景
+static const char col_fg_s[]       = "#fffffb";
+static const char col_bg_s[]       = "#6486aa";
+static const char col_border_s[]       = "#6486aa";
+
+//dmenu色
+static const char col_dmenu_fg[]       = "#bbbbbb";
+static const char col_dmenu_fg_s[]       = "#eeeeee";
+static const char col_dmenu_bg_s[]       = "#6486aa";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg, col_bg, col_border },
+	[SchemeSel]  = { col_fg_s, col_bg_s, col_border_s  },
 };
-
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -57,7 +73,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_dmenu_fg, "-sb", col_dmenu_bg_s, "-sf", col_dmenu_fg_s, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
--- dwm/install.sh
+++ dwm/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+pid=$(ps -ef | grep dwm | grep -v grep | awk '{print $2}')
+[[ -n "${pid}" ]] && kill -9 ${pid}
 [ -d "~/soft/dwm" ] || rm -rf ~/soft/dwm
 mkdir -p ~/soft/dwm
+cp ./.xinitrc ~/.xinitrc
 cp -rf ./dwm/* ~/soft/dwm
 cd ~/soft/dwm
 # ls
