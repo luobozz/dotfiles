ded6f221535749aedf1cbaf4e6975832396f2752
luobozz <437337961@qq.com>
2022-02-07 11:59:16 +0800

    heath_notice

--- windows/计划任务/heath_notice.bat
+++ windows/计划任务/heath_notice.bat
@@ -1,1 +1,2 @@
-pwsh.exe -command New-BurntToastNotification -AppLogo .\water.png -Text '喝水喝水' -Sound 'Alarm2' -SnoozeAndDismiss
+@echo off
+pwsh.exe -command New-BurntToastNotification -AppLogo .\water.png -Text '起来喝水喝水' -Sound 'Alarm2' -SnoozeAndDismiss
---
+++ windows/计划任务/heath_notice.vbs
@@ -0,0 +1,2 @@
+Set shell = Wscript.createobject("wscript.shell")
+a = shell.run (".\heath_notice.bat",0)
