6fff60ee4688ebb3449887c3828fd0e02c3529a8
luobo <437337961@qq.com>
2022-05-20 18:16:37 +0800

    dwmblock with xinit.d

--- linux/distribution/arch/.xinitrc.d/statusline.sh
+++ linux/distribution/arch/.xinitrc.d/statusline.sh
@@ -7,10 +7,10 @@ function acpi_check() {
     acpi_info=$(acpi -b 2>/dev/null | awk '{ print $3, $4 }')
     [[ -z "${acpi_info}" ]] && {
         echo " Full"
-    } || {
+        } || {
         case "${acpi_info}" in
-        *)
-            echo "default"
+            *)
+                echo "default"
             ;;
         esac
     }
@@ -20,10 +20,10 @@ function vol_check() {
     vol_info=$(pamixer --get-volume 2>>/dev/null)
     [[ -z "${vol_info}" ]] && {
         echo " 100"
-    } || {
+        } || {
         if [ "$vol" -gt "70" ]; then
             echo " $vol"
-        elif [ "$vol" -lt "30" ]; then
+            elif [ "$vol" -lt "30" ]; then
             echo " $vol"
         else
             echo " $vol"
@@ -51,32 +51,44 @@ function nettf_check() {
     #     printf %d\\n "$sum" >"$cache"
     #     printf %d\\n $((sum - old))
     # }
-
+    
     # rx=$(update /sys/class/net/[ew]*/statistics/rx_bytes)
     # tx=$(update /sys/class/net/[ew]*/statistics/tx_bytes)
-
+    
     # # echo " $rx  $tx"
     echo ""
 }
 
-# item_array=("nettf_check" "cpu_check" "mem_check" "vol_check" "acpi_check" "date_check")
-item_array=("mem_check" "vol_check" "acpi_check" "date_check")
 
-# echo "${status_str}"
 
-while true; do
-    status_str=" "
-    for ((i = 0; i < ${#item_array[*]}; i++)); do
-        out="$(${item_array[i]})"
-        [[ -n "${out}" ]] && {
-            status_str="${status_str}${out}"
-        }
-        let index=${#item_array[*]}-1
-        ((${i} != ${index})) && {
-            status_str="${status_str} | "
-        }
-    done
-    xsetroot -name "${status_str} "
+function status_bar_xsetroot(){
+    # item_array=("nettf_check" "cpu_check" "mem_check" "vol_check" "acpi_check" "date_check")
+    item_array=("mem_check" "vol_check" "acpi_check" "date_check")
+    
     # echo "${status_str}"
-    sleep 1s
-done
+    
+    while true; do
+        status_str=" "
+        for ((i = 0; i < ${#item_array[*]}; i++)); do
+            out="$(${item_array[i]})"
+            [[ -n "${out}" ]] && {
+                status_str="${status_str}${out}"
+            }
+            let index=${#item_array[*]}-1
+            ((${i} != ${index})) && {
+                status_str="${status_str} | "
+            }
+        done
+        xsetroot -name "${status_str} "
+        # echo "${status_str}"
+        sleep 1s
+    done
+}
+
+function status_dwm_block(){
+    xsetroot -name "dwmblocks loading..."
+    dwmblocks
+}
+
+status_dwm_block
+# status_bar_xsetroot
