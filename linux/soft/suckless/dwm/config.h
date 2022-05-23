0a0be7fc43a989a2a2a50cb8e1b8865d11a6fa4c
luobo <437337961@qq.com>
2022-05-23 23:45:44 +0800

    chinese fonts for dwmblocks

--- linux/soft/suckless/dwm/config.h
+++ linux/soft/suckless/dwm/config.h
@@ -16,7 +16,7 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 // static char *fonts[]          = { "Fira Mono:style=Bold:size=10","HarmonyOS Sans SC:style=Bold:size=10" };
-static char *fonts[]          = {"JetBrains Mono:style=Bold:pixelsize=14:antialias=true:autohint=true","Fira Code Nerd Font Mono:pixelsize=20:antialias=true:autohint=true"};
+static char *fonts[]          = {"JetBrains Mono:style=Bold:pixelsize=14:antialias=true:autohint=true"," Alibaba PuHuiTi 2.0:style=Bold:pixelsize=14" ,"Fira Code Nerd Font Mono:pixelsize=20:antialias=true:autohint=true"};
 static const char dmenufont[]       = "Fira Mono:style=Bold:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
