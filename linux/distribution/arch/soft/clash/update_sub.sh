a13f05f4e769834e8b4ffd0b9b2d33f6c8ff5ff8
luobo <437337961@qq.com>
2022-05-25 10:12:25 +0800

    hide my regurl

--- linux/distribution/arch/soft/clash/update_sub.sh
+++ linux/distribution/arch/soft/clash/update_sub.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl 'https://new.ssrss.de/sub?target=clash&new_name=true&url=https%3A%2F%2Fssrss.de%2Fs%2FCTFRHMSFWUSA&udp=true&tfo=true&config=https%3A//ssrss.de/ssrss.ini' 1>~/soft/clash/config.yaml 2>/dev/null
+curl '' 1>~/soft/clash/config.yaml 2>/dev/null
