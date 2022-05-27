e78809a28dfd4c69c33cf7cf64dd9116778b10c0
luobo <437337961@qq.com>
2022-05-27 14:41:48 +0800

    wallpapers & some scripts

--- linux/distribution/arch/.xinitrc.d/.xinitrc.d/wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
-  feh --recursive --randomize --bg-fill ~/pictures/wallpapers/
-  sleep 3m
-done
--- linux/distribution/arch/.xinitrc.d/wallpaper.sh
+++ linux/distribution/arch/.xinitrc.d/wallpaper.sh
@@ -2,5 +2,5 @@
 while true
 do
   feh --recursive --randomize --bg-fill ~/pictures/wallpapers/
-  sleep 3m
+  sleep 2m
 done
