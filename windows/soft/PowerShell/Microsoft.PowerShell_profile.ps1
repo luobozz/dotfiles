f549d98e49a369f9da70c9309cc9749c11deb328
luobozz <437337961@qq.com>
2022-09-27 09:26:17 +0800

    utools

--- windows/soft/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/soft/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,13 @@
-Import-Module posh-git # 引入 posh-git
-Import-Module oh-my-posh # 引入 oh-my-posh
-
-Set-PoshPrompt zash
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/zash.omp.json" | Invoke-Expression
 
 Set-PSReadLineOption -PredictionSource History # 设置预测文本来源为历史记录
  
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete # 设置 Tab 键补全
 Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
 
-$env:HTTP_PROXY="http://localhost:10809"
-$env:HTTPS_PROXY="http://localhost:10809"
+#$env:HTTP_PROXY="http://localhost:10809"
+#$env:HTTPS_PROXY="http://localhost:10809"
+
+Set-Alias -Name c clear
+Set-Alias -Name sc scoop
+Set-Alias -Name nf neofetch
